5fc24bee6d9167461b0f474a7da5b33bd38f16ad
Kirill Yuzeev <aarnell@student.21-school.ru>
2022-06-24 21:52:48 +0300

    Initial commit

---
+++ src/day00/ex01/Program.java
@@ -0,0 +1,13 @@
+package day00.ex01;
+
+public class Program {
+    public static void main(String[] args) {
+        int a = 479598;
+        int sum = 0;
+        while (a > 0) {
+            sum += a % 10;
+            a /= 10;
+        }
+        System.out.println(sum);
+    }
+}
