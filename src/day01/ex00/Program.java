31c195da3e83d894a5d7d11e2e9d703de0bd9feb
lavrenovamaria <wrickard@student.21-school.ru>
2022-07-09 13:22:33 +0300

    feat: New user John, null with ID Exception

--- src/day01/ex00/Program.java
+++ src/day01/ex00/Program.java
@@ -2,12 +2,24 @@ package day01.ex00;
 
 import java.util.Scanner;
 
-public class Program {
+public class Program{
     public static void main(String[] args) {
         //init Scanner
-        Scanner sc = new Scanner(System.in);
-        User currentUser = null;
-        while (true)
-            currentUser = Program.mainMenuPrompt(sc);
+        //Scanner sc = new Scanner(System.in);
+        //init user
+        User userSender = new User("John", 100);
+        User userReceiver = new User("Tommy", 1000);
+        //init transaction
+        String category = "debit";
+        Integer amount = 100;
+        Transaction transaction = new Transaction();
+        transaction.setCategory(category);
+        transaction.setAmount(amount);
+        //print transaction
+        System.out.println(transaction.getSender().getName() + " -> "  + transaction.getRecipient().getName() + ", " + transaction.getAmount() + ", " + transaction.getCategory() + ", " + transaction.getUUID());
     }
+
+    //public static User mainMenuPrompt(Scanner sc) {
+
+
 }
