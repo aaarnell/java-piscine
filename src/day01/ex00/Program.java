393690ba6977a71762ccad45c52f8c8f85078a13
lavrenovamaria <wrickard@student.21-school.ru>
2022-07-09 20:53:27 +0300

    feat: i wish it is done

--- src/day01/ex00/Program.java
+++ src/day01/ex00/Program.java
@@ -5,36 +5,40 @@ import java.util.Scanner;
 //credit is all the money flowing OUT an account
 public class Program{
     public static void main(String[] args) {
-        //init Scanner
-        //Scanner sc = new Scanner(System.in);
-        //init user
-        System.out.println("\n======================TOMMY'S CREDIT========================");
+        System.out.println("\n======================TOMMY to JOHNY========================");
         User userTommy = new User("Tommy", 1000);
-        System.out.println("User: " + userTommy.getName() + " " + userTommy.getBalance());
+        User userJohn = new User("John", 200);
+
+        System.out.println("Sender: " + userTommy.getName() + " with balance" + " " + userTommy.getBalance());
+        System.out.println("Receiver: " + userJohn.getName() + " with balance" + " " + userJohn.getBalance());
         //init transaction
         String category = "credit";
         Integer amount = -100;
         Transaction transaction = new Transaction();
         transaction.setCategory(category);
         transaction.setAmount(amount);
+
         userTommy.setBalance(userTommy.getBalance() + amount);
-        //print transaction
+        userJohn.setBalance(userJohn.getBalance() + (-amount));
+
         System.out.println("Transaction type: " + transaction.getCategory() + " " + transaction.getAmount());
         System.out.println("After transaction: " + userTommy.getName() + " " + userTommy.getBalance());
-        System.out.println("\n=======================JOHNY'S DEBIT========================");
-        User userJohn = new User("John", 200);
-        System.out.println("Sender: " + userJohn.getName() + " " + userJohn.getBalance());
+        System.out.println("After transaction: " + userJohn.getName() + " " + userJohn.getBalance());
+        System.out.println("\n======================JOHNY to TOMMY========================");
+
+        System.out.println("Receiver: " + userTommy.getName() + " with balance" + " " + userTommy.getBalance());
+        System.out.println("Sender: " + userJohn.getName() + " with balance" + " " + userJohn.getBalance());
         category = "debit";
         amount = 200;
         transaction.setCategory(category);
         transaction.setAmount(amount);
-        userJohn.setBalance(userJohn.getBalance() + amount);
+
+        userJohn.setBalance(userJohn.getBalance() + (-amount));
+        userTommy.setBalance(userTommy.getBalance() + amount);
+
         System.out.println("Transaction type: " + transaction.getCategory() + " " + transaction.getAmount());
+        System.out.println("After transaction: " + userTommy.getName() + " " + userTommy.getBalance());
         System.out.println("After transaction: " + userJohn.getName() + " " + userJohn.getBalance());
 
     }
-
-    //public static User mainMenuPrompt(Scanner sc) {
-
-
 }
