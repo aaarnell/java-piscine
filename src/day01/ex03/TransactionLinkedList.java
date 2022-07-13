47bd0256c6b14fa034b9d4a582e13733b5d3f15e
lavrenovamaria <wrickard@student.21-school.ru>
2022-07-13 18:46:52 +0300

    feat: ex03 on the way

--- src/day01/ex03/TransactionLinkedList.java
+++ src/day01/ex03/TransactionLinkedList.java
@@ -1,9 +1,19 @@
 package day01.ex03;
 
 public class TransactionLinkedList implements TransactionList {
+
 	private Transaction first;
+
 	private Transaction last;
+
 	private int size;
+
+	/**
+	 * @param first first element of the list
+	 * @param last last element of the list
+	 * @param size number of elements in the list
+	 */
+
 
 	public TransactionLinkedList() {
 		this.first = null;
@@ -15,5 +25,62 @@ public class TransactionLinkedList implements TransactionList {
 
 	}
 
+	public void removeById(UUID uuid) throws TransactionNotFound{
+
+
+	}
+
+	public Transaction[] toArray(){
+		return null;
+	}
+
+	private static class transactionNode {
+		private Transaction transaction;
+		private transactionNode next;
+		private transactionNode prev;
+
+		/**
+		 * @param transaction transaction data
+		 * @param next pointer to next transaction
+		 * @param prev pointer to previous transaction
+		 */
+
+		public transactionNode(Transaction transaction, transactionNode next, transactionNode prev) {
+			this.transaction = transaction;
+			this.next = next;
+			this.prev = prev;
+		}
+
+		//get transaction data
+		public Transaction getTransaction() {
+			return transaction;
+		}
+
+		//get next transaction
+		public transactionNode getNext() {
+			return next;
+		}
+
+		public transactionNode getPrev() {
+			return prev;
+		}
+
+		public void setTransaction(Transaction transaction) {
+			this.transaction = transaction;
+		}
+
+		public void setNext(transactionNode next) {
+			this.next = next;
+		}
+
+		public void setPrev(transactionNode prev) {
+			this.prev = prev;
+		}
+
+		public String toString() {
+			return transaction.toString();
+		}
+
+	}
 
 }
