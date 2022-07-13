bdda4f8eb23da1c808a63f77b20d68edae986d79
lavrenovamaria <wrickard@student.21-school.ru>
2022-07-13 21:41:31 +0300

    feat: ex03 done

--- src/day01/ex03/Program.java
+++ src/day01/ex03/Program.java
@@ -6,15 +6,21 @@ public class Program {
 			User userTommy = new User("Tommy", 2000);
 			User userJohn = new User("John", 1000);
 			// Create a transaction
-			Transaction transaction = Transaction.createTransaction(userTommy, userJohn, 100);
+			//Transaction transaction = Transaction.createTransaction(userTommy, userJohn, -100);
+			Integer amount = 100;
+			Transaction outcome = Transaction.createTransaction(userTommy, userJohn, -amount);
+        	Transaction income = Transaction.createTransaction(userTommy, userJohn, amount);
 			TransactionList transactionList = new TransactionLinkedList();
 			//ссылка типа интерфейса TransactionList ссылается на новый объект типа TransactionLinkedList
 			//add - добавляет транзакцию в список
-			transactionList.add(transaction);
+			transactionList.add(outcome);
+			transactionList.add(income);
 			System.out.println("Transaction list size: " + ((TransactionLinkedList) transactionList).getSize());
-			transactionList.removeById(transaction.getUUID());
+			transactionList.removeById(outcome.getUUID());
+			transactionList.removeById(income.getUUID());
 
-			System.out.println("Transaction: " + transaction.getUUID() + " " + "from "+ transaction.getSender().getName() + " " + "to "+ transaction.getRecipient().getName() + " " + "with "+ transaction.getCategory() + " " + transaction.getAmount());
+			System.out.println("Transaction: " + outcome.getUUID() + " " + "from "+ outcome.getSender().getName() + " " + "to "+ outcome.getRecipient().getName() + " " + "with "+ outcome.getCategory() + " " + outcome.getAmount());
+			System.out.println("Transaction: " + income.getUUID() + " " + "from "+ income.getSender().getName() + " " + "to "+ income.getRecipient().getName() + " " + "with "+ income.getCategory() + " " + income.getAmount());
 			System.out.println("Transaction list size after removal: " +((TransactionLinkedList) transactionList).getSize());
 
 			System.out.println("After transaction: " + userTommy.getName() + " " + userTommy.getBalance());
