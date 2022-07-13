c8589bfd077d5016d2a73dc42ab2da3878c43d72
lavrenovamaria <wrickard@student.21-school.ru>
2022-07-13 21:22:44 +0300

    feat: ex03 done

--- src/day01/ex03/Program.java
+++ src/day01/ex03/Program.java
@@ -3,24 +3,22 @@ package day01.ex03;
 public class Program {
 	public static void main(String[] args) {
 		try {
-			User userTommy = new User("Tommy", 12345);
-			User userJohn = new User("John", 12345);
+			User userTommy = new User("Tommy", 2000);
+			User userJohn = new User("John", 1000);
+			// Create a transaction
 			Transaction transaction = Transaction.createTransaction(userTommy, userJohn, 100);
 			TransactionList transactionList = new TransactionLinkedList();
 			//ссылка типа интерфейса TransactionList ссылается на новый объект типа TransactionLinkedList
+			//add - добавляет транзакцию в список
 			transactionList.add(transaction);
-			System.out.println(((TransactionLinkedList) transactionList).getSize());
+			System.out.println("Transaction list size: " + ((TransactionLinkedList) transactionList).getSize());
 			transactionList.removeById(transaction.getUUID());
-			//init transaction
-			Integer amount = 1001;
-			transaction = Transaction.createTransaction(userTommy, userJohn, amount);
-			System.out.println("Transaction: " + transaction.getUUID() + " " + transaction.getSender().getName() + " " + transaction.getRecipient().getName() + " " + transaction.getCategory() + " " + transaction.getAmount());
-			System.out.println(((TransactionLinkedList) transactionList).getSize());
-			//add transaction to list
-//			transactionList = new TransactionLinkedList();
-//			transactionList.add(transaction);
-//			//remove transaction from list
-//			transactionList.removeById(transaction.getUUID());
+
+			System.out.println("Transaction: " + transaction.getUUID() + " " + "from "+ transaction.getSender().getName() + " " + "to "+ transaction.getRecipient().getName() + " " + "with "+ transaction.getCategory() + " " + transaction.getAmount());
+			System.out.println("Transaction list size after removal: " +((TransactionLinkedList) transactionList).getSize());
+
+			System.out.println("After transaction: " + userTommy.getName() + " " + userTommy.getBalance());
+        	System.out.println("After transaction: " + userJohn.getName() + " " + userJohn.getBalance());
 
 		} catch (TransactionNotFound e) {
 			System.out.println(e.getMessage());
