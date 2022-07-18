b81f352f0b38d835e0dd9f9c804799798984762c
lavrenovamaria <wrickard@student.21-school.ru>
2022-07-18 17:12:16 +0300

    feat: day02ex00 fos done

--- src/day02/ex00/Program.java
+++ src/day02/ex00/Program.java
@@ -1,14 +1,13 @@
 package day02.ex00;
 
 import java.io.*;
-//import java.util.HexFormat;
 import java.util.*;
 
 //read from file /Users/wrickard/Desktop/java-piscine/src/day02/ex00/signatures.txt
-	///Users/wrickard/Desktop/java-piscine/src/day02/ex00/someshit.txt
+//city-sign-design-mockup.zip
+	///Users/wrickard/Desktop/java-piscine/src/day02/ex00/5800895.jpg
 	///Users/wrickard/Desktop/java-piscine/src/day02/ex00/screen.png
-	//screen.png
-
+///Users/wrickard/Desktop/java-piscine/src/day02/ex00/city-sign-design-mockup.zip
 public class Program {
 	public static void main(String[] args) throws IOException {
 		Map<String, String> signatures = new HashMap<>();//create a map to store the signatures
@@ -27,79 +26,40 @@ public class Program {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-
 		StringBuilder res = new StringBuilder();
-
-		try {
-			StringBuilder sb = new StringBuilder();
-			FileInputStream fis = new FileInputStream("/Users/wrickard/Desktop/java-piscine/src/day02/ex00/5800895.jpg");
-			for (int i = 0; i < 8; i++) {
-				if (i > 0) {
-					sb.append(' ');
-				}
-				String tmp = Integer.toHexString(fis.read());
-				if (tmp.length() == 1) {
-					sb.append('0');
-				}
-				sb.append(tmp.toUpperCase());
-				if (signatures.containsKey(sb.toString())) {
-					res.append(signatures.get(sb.toString()));
-					break;
-				}
-			}
-			if(res.length() == 0)
-				res.append("UNDEFINED");
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		System.out.println(res);
-
-
-/*
 		Scanner scan = new Scanner(System.in);//create a scanner to read from the console
 		String path;
-		StringBuilder sb = new StringBuilder();//create a string builder to store the signatures
+		FileOutputStream fos = new FileOutputStream("src/day02/ex00/result.txt");
 		while (scan.hasNext()) {
-			path = scan.next();//get the path from the console
-			if (path.equals("42")) {//if the path is end, break the loop
+			path = scan.next();
+			if (path.equals("42")) {
 				break;
 			}
-			int maxCount = -1;//create a max counter to store the max count of the signature
-			try (FileInputStream fis = new FileInputStream(path);//create a file input stream to read from the file
-				 FileOutputStream fos = new FileOutputStream("src/day02/ex00/result.txt")) {//create a file output stream to write to the file
- 				int a;
- 				while ((a = fis.read()) != -1) {//read from the file
- 					sb.append((char) a);//append the character to the string builder
- 				}
-				String str = sb.toString();//convert the string builder to a string
-				for (String key : signatures.keySet()) {//loop through the keys in the map
-					int count1 = 0;//create a counter to count the number of times the signature appears in the string
-					for (int i = 0; i < str.length(); i++) {//loop through the string
-						if (str.charAt(i) == signatures.get(key).charAt(0)) {//if the character at the current index is the same as the first character of the signature
-							count1++;//increment the counter
-							if (count1 > maxCount) {//if the counter is greater than the max count
-								maxCount = count1;//set the max count to the counter
-								//удалить с 47 по 50
-								//пройтись по строке с путем и забрать что после точки
-								//потом сравнить это с сигнатурами в файле
-							}
-						}
+			try {
+				StringBuilder sb = new StringBuilder();
+				FileInputStream fis = new FileInputStream(path);
+				for (int i = 0; i < 8; i++) {
+					if (i > 0) {
+						sb.append(' ');
+					}
+					String tmp = Integer.toHexString(fis.read());
+					if (tmp.length() == 1) {
+						sb.append('0');
+					}
+					sb.append(tmp.toUpperCase());
+					if (signatures.containsKey(sb.toString())) {
+						res.append(signatures.get(sb.toString()));
+						//res.append("\n");
+						fos.write(signatures.get(sb.toString()).getBytes());
+						fos.write("\n".getBytes());
+						break;
 					}
 				}
-				for(HashMap.Entry<String, String> entry : signatures.entrySet()) {//loop through the map
-					String key = entry.getKey();//get the key
-					//String value = entry.getValue();
-					if(sb.toString().toUpperCase().startsWith(key)) {//if the string contains the key
-						for (int i = 0; i < key.length(); i++) {//loop through the key
-                        	fos.write(key.charAt(i));//write the character to the file
-                        }
-						System.out.println("PROCESSED");
-                               break;
-					}
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
+				System.out.println("PROCESSED");
+			} catch (FileNotFoundException e) {
+				System.out.println("UNDEFINED");
+				System.exit(-1);
 			}
-		}*/
 	}
+}
 }
