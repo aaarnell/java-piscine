8bea3b18aed75f53ee83d9a03c8718a73bbe27f0
lavrenovamaria <wrickard@student.21-school.ru>
2022-07-22 23:02:36 +0300

    feat: day04ex01 done

---
+++ src_/day04/ex01/ImagesToChar/src/java/edu/school21/printer/app/Program.java
@@ -0,0 +1,24 @@
+package edu.school21.printer.app;
+
+import edu.school21.printer.logic.Logic;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Program {
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2)
+            throw new Exception("Number of arguments is incorrect");
+
+        char whiteColor = args[0].charAt(0);
+        char blackColor = args[1].charAt(0);
+
+        List<String> arrayFromFile = Logic.readImage(Paths.get("./target/resources/it.bmp"), blackColor, whiteColor);
+
+        for (String line : arrayFromFile)
+            System.out.println(line);
+    }
+}
