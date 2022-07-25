08cb16de6634dd47020c249082e2d98a90df8162
Kirill Yuzeev <aarnell@student.21-school.ru>
2022-07-25 22:38:06 +0300

    fix: d05ex01

--- src_/day05/ex00/Chat/src/main/java/edu/school21/chat/app/Program.java
+++ src_/day05/ex00/Chat/src/main/java/edu/school21/chat/app/Program.java
@@ -1,8 +1,12 @@
 package edu.school21.chat.app;
 
 import com.zaxxer.hikari.HikariDataSource;
+import edu.school21.chat.models.Message;
+import edu.school21.chat.repositories.MessagesRepository;
+import edu.school21.chat.repositories.MessagesRepositoryJdbcImpl;
 
 import java.sql.SQLException;
+import java.util.Optional;
 
 public class Program {
 	public static void main(String[] args) throws SQLException {
@@ -12,9 +16,14 @@ public class Program {
 		Это синглтон, поэтому можно получить объект из любого места программы.
 		*/
 
-		dataSource.setJdbcUrl("");//путь к базе данных
+		dataSource.setJdbcUrl("jdbc:postgresql://localhost:5433/postgres");//путь к базе данных
 		dataSource.setUsername("");//логин пользователя
 		dataSource.setPassword("");//пароль пользователя
 
+		MessagesRepository messageRepo = new MessagesRepositoryJdbcImpl(dataSource);
+
+		Optional<Message> message = messageRepo.findById(3L);
+		System.out.println(message.toString());
+		dataSource.close();
 	}
 }
