a25ea6f460e1c6c17ff1e0cdf9cee838801b9c30
wrickard <wrickard@student.21-school.ru>
2022-07-29 22:30:35 +0300

    feat: day08 progress

---
+++ src_/day08/ex01/Service/src/main/java/school21/spring/service/repositories/CrudRepository.java
@@ -0,0 +1,5 @@
+package school21.spring.service.repositories;
+
+public interface CrudRepository <T> {
+
+}
---
+++ src_/day08/ex01/Service/src/main/java/school21/spring/service/repositories/UsersRepository.java
@@ -0,0 +1,7 @@
+package school21.spring.service.repositories;
+
+import school21.spring.service.models.User;
+
+public interface UsersRepository extends CrudRepository<User> {
+
+}
